939702c829f7b9a24f790e0c2c29def6b3930dd5
Hafsa Tariq Ali <137608362+hafsahtariqali@users.noreply.github.com>
2024-08-14 22:20:25 +0500

    removed waitlist banner

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,3 @@
-import  Banner from "../components/Banner";
 import  NavBar  from "../components/NavBar";
 import  Hero  from "../components/Hero";
 import  {Features} from "../components/Features";
@@ -9,7 +8,6 @@ import  Footer  from "../components/Footer";
 export default function Home() {
   return (
     <>
-      <Banner />
       <NavBar />
       <Hero />
       <Features />
